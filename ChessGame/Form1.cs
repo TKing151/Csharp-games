018a3b57e49c6bbf05190efd55e343218d7f9563
Timothy King <64551677+TKing151@users.noreply.github.com>
2021-12-19 00:35:46 -0500

    Add files via upload

---
+++ ChessGame/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PA6_Draft
+{
+    public partial class Form1 : Form
+    {
+        public int promo = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            promo = 0;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            promo = 1;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            promo = 2;
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            promo = 3;
+        }
+    }
+}
